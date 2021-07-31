68338159ee450f91d8fa000154a0e87315603960
Muhammad Ibad Saleem <34434738+ibadsaleem@users.noreply.github.com>
2021-08-01 01:05:44 +0500

    Login

--- App.js
+++ App.js
@@ -1,23 +1,51 @@
 import React, { useState } from 'react'
 import { View, SafeAreaView, Text, StyleSheet, TouchableOpacity, Touchable, Button, Pressable, Alert, TextInput, ImageBackground } from 'react-native'
 export default function App() {
-  // const image = { require('Backgrounds\pexels-adrianna-calvo-364096.jpg') };
+ const [email,setemail]=useState('');
+ const [password,setpassword]=useState('');
 
+
+ const handle_Email=(data)=>{
+   console.log(data);
+   setemail(data);
+ }
+ const handle_Password=(data)=>{
+   console.log(data);
+   setpassword(data);
+ }
+
+ const handle_Button=()=>{
+   if (email!='' && password!='')
+   {
+     Alert.alert('Logged In');
+   }
+   else if (email=='' && password=='')
+   {
+    Alert.alert("Email and Password are Missing");
+   }
+   else if (password==''){
+     Alert.alert("Password is Missing");
+   }
+   else {
+    Alert.alert("Email is Missing");
+   }
+  
+ }
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.text} >React<Text style={styles.text1}>App</Text></Text>
       <View style={styles.box}>
-        <TextInput style={styles.input} placeholder={"Enter Email"} placeholderTextColor="black"></TextInput>
+        <TextInput style={styles.input} onChangeText={handle_Email} placeholder={"Enter Email"} placeholderTextColor="black"></TextInput>
       </View>
       <View style={styles.box}>
-        <TextInput style={styles.input} placeholder={"Enter Password"} placeholderTextColor="black" ></TextInput>
+        <TextInput style={styles.input} onChangeText={handle_Password} placeholder={"Enter Password"} secureTextEntry={true} placeholderTextColor="black" ></TextInput>
       </View>
-      <Text style={{ textAlign: 'center', color: 'white', padding: 10 }}>Forgot Password?</Text>
+      <Pressable onPress={()=>{Alert.alert("Forgot Password")}}><Text style={{ textAlign: 'center', color: 'white', padding: 10 }}>Forgot Password?</Text></Pressable>
       <View>
         <Pressable style={styles.button}>
           <Button
             title="Log in"
-            onPress={() => Alert.alert("Logged In")}
+            onPress={handle_Button}
           />
         </Pressable>
       </View>
@@ -37,7 +65,7 @@ const styles = StyleSheet.create({
     fontSize: 50,
     color: "red",
     marginTop: 200,
-    marginBottom: 40,
+    marginBottom: 40
     
   },
   text1:{alignSelf: 'center',
@@ -51,7 +79,7 @@ const styles = StyleSheet.create({
   input: {
     borderWidth: 1,
     borderColor: 'white',
-    color: 'white',
+    color: 'black',
     borderRadius: 20,
     paddingLeft: 20,
     backgroundColor: 'white',
@@ -63,7 +91,7 @@ const styles = StyleSheet.create({
   button: {
     width: 200,
     alignSelf: "center",
-    padding: 10,
+    padding: 10
 
 
 
